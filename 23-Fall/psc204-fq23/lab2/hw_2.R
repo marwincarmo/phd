bca3f37d9dd57c915f3b4dbd6a0552520258f47a
Marwin Carmo <marwinbr@gmail.com>
2023-10-10 17:26:09 -0700

    204 hw2

---
+++ 23-Fall/psc204-fq23/lab2/hw_2.R
@@ -0,0 +1,28 @@
+
+
+# 1. Import data ----------------------------------------------------------
+
+sat_data <- read.csv("data/sat_act.csv")
+sat_data
+
+# 2. Class of dataset columns ---------------------------------------------
+
+str(sat_data)
+
+# 3. Mean and median of continuous variables ------------------------------
+
+# Identify the continuous variables
+continuous_variables <- c("age", "ACT", "SATV", "SATQ")
+
+# Calculate the mean and median of each continuous variable
+summary(sat_data[continuous_variables])
+
+
+# 4. Standard deviation of continuous variables ---------------------------
+
+
+sd(sat_data$age, na.rm = TRUE)
+sd(sat_data$ACT, na.rm = TRUE)
+sd(sat_data$SATV, na.rm = TRUE)
+sd(sat_data$SATQ, na.rm = TRUE)
+
